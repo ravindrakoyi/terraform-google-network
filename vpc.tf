43988e77bd153138271cb9c1e3c39063dc55d312
ravindrakoyi <38636580+ravindrakoyi@users.noreply.github.com>
2021-11-03 13:21:05 -0400

    Delete vpc.tf

--- vpc.tf
+++
@@ -1,9 +0,0 @@
-network_name="ravi-network"
-project-id="var.project"
-subnets = [
-  {
-    subnet_name   = "ravi-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]
